bc3fa19b7fcdc4f623c753acf8d69c99cc56db72
daiwk <daiwk1989@gmail.com>
2025-06-04 19:50:59 +0800

    switch to qwen via lm studio

--- backend/src/agent/configuration.py
+++ backend/src/agent/configuration.py
@@ -1,29 +1,29 @@
 import os
-from pydantic import BaseModel, Field
 from typing import Any, Optional
 
 from langchain_core.runnables import RunnableConfig
+from pydantic import BaseModel, Field
 
 
 class Configuration(BaseModel):
     """The configuration for the agent."""
 
     query_generator_model: str = Field(
-        default="gemini-2.0-flash",
+        default="Qwen1.5-72B-Chat",
         metadata={
             "description": "The name of the language model to use for the agent's query generation."
         },
     )
 
     reflection_model: str = Field(
-        default="gemini-2.5-flash-preview-04-17",
+        default="Qwen1.5-72B-Chat",
         metadata={
             "description": "The name of the language model to use for the agent's reflection."
         },
     )
 
     answer_model: str = Field(
-        default="gemini-2.5-pro-preview-05-06",
+        default="Qwen1.5-72B-Chat",
         metadata={
             "description": "The name of the language model to use for the agent's answer."
         },
